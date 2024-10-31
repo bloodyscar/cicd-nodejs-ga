1c19b4ed9be844042c392745e19624bb168652b8
Adit <adithernowo@gmail.com>
2024-10-31 14:05:16 +0700

    youtube

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('This App is running properly!'))
 app.get('/ping', (req, res) => res.send('Pong!'))
-app.get('/youtube', (req, res) => res.send('Hello, youtube indonesia!'))
+app.get('/youtube', (req, res) => res.send('Hello, youtube indonesiaaaa!'))
 
 module.exports = app
