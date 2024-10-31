baa627cb04daffce509b8289b64bf7a973d67c6e
Adit <adithernowo@gmail.com>
2024-10-31 13:59:26 +0700

    first

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('This App is running properly!'))
+app.get('/ping', (req, res) => res.send('Pong!'))
+app.get('/youtube', (req, res) => res.send('Hello, youtube indonesia!'))
+
+module.exports = app
